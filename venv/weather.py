7eed5461c19f14f42d95dc3bbf1d16e31a68755a
Leopold Akpabio <leopoldakpabio@yahoo.com>
2022-02-07 02:25:33 +0000

    commit

---
+++ venv/weather.py
@@ -0,0 +1,26 @@
+import requests
+
+def get_weather_desc_and_temp():
+    api_key = "d296751e6647044a94b930e0ed830268"
+    city = "Orlando"
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=imperial"
+
+    request = requests.get(url)
+    json = request.json()
+
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
+
+    return {'description': description,
+            'temp_min': temp_min,
+            'temp_max': temp_max}
+
+def main():
+    # Print the results
+    weather_dict = get_weather_desc_and_temp()
+    print("Today's forecast is", weather_dict.get('description'))
+    print("The minimum temperature is ", weather_dict.get('temp_min'))
+    print("The maximum temperature is ", weather_dict.get('temp_max'))
+
+main()
